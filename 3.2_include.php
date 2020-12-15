fa72e3ed782e662c96a4f613f758c2f64dafaf0f
noumanbhai <noumanbhai006@gmail.com>
2020-12-15 00:57:38 -0800

    PHP Include and Require Files
    PHP Include Files

---
+++ 3.2_include.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<h1>Hellow Wrold</h1>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis sequi debitis quam, nostrum quasi veniam sed ut beatae dolores animi magnam similique saepe voluptatibus at? Ea, repudiandae, nemo. Quo, explicabo!</p>
+
+
+
+
+
+	<h4>include is used in footer</h4>
+	<h4>require is used in footer</h4>
+
+	<?php 
+
+	include('footer.php');
+// require('footer.php');
+	?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<Footer style="background-color: black;color:white;text-align:center;">
+	
+	&copy 2010 to- 20<?php echo date('y') ?>
+
+</Footer>
